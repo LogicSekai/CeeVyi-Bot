02d7f3d155093aa6d78b03b02ef0aa82c89a6804
LogicSekai <cameliayu112@gmail.com>
2023-04-24 00:54:19 +0700

    Create readme file

--- deploy/code.js
+++ deploy/code.js
@@ -1,18 +1,6 @@
 const { exec } = require('child_process');
 const readline = require('readline');
 
-exec('git add .', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error executing command: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.error(`Error output: ${stderr}`);
-        return;
-    }
-    console.log(`Command output: ${stdout}`);
-});
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
